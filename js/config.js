6c3e9c90296e228685b0b77f8ed320e687aae251
olittleharmony <olittleharmony@gmail.com>
2026-04-21 16:25:52 +0700

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,9 +1,9 @@
 const APP_CONFIG = {
-  SUPABASE_URL:      'https://XXXX.supabase.co',
-  SUPABASE_ANON_KEY: 'eyJhbGc...',
-  GAS_URL:           'https://script.google.com/macros/s/XXXX/exec',
+  SUPABASE_URL:      'https://tovitqcgmsmohbjmaifk.supabase.co/rest/v1/',
+  SUPABASE_ANON_KEY: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InRvdml0cWNnbXNtb2hiam1haWZrIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzY3NTk3NzksImV4cCI6MjA5MjMzNTc3OX0.m8PsmN_B6XnI5IHx_n-_sBPUy4g_Es8uX8-chgnlSqc',
+  GAS_URL:           'https://script.google.com/macros/s/AKfycbwplBjReoLeOI5I2bzkUjQMZfK6Yc5CKyywm8-yFhyV6vQJD66nWIS7lpo88ujum6o0/exec',
   APP_NAME:          'LHO Finance',
   ORG_NAME:          'Little Harmony Orchestra',
-  APP_URL:           'https://USERNAME.github.io/lho-finance',
+  APP_URL:           'https://olittleharmony.github.io/lho-finance',
 };
 Object.freeze(APP_CONFIG);
